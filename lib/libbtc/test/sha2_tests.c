c493ec909559edf8dc9d77048c265396b2ae2019
Anton Ehrmanntraut <anton@ehrmanntraut.de>
2019-10-03 15:47:36 +0200

    Korrigiere Inkompatibilitäten der Bezeichner zw. OpenSSL/libbtc

--- lib/libbtc/test/sha2_tests.c
+++ lib/libbtc/test/sha2_tests.c
@@ -412,7 +412,7 @@ static const struct sha_hmac_test_v sha_hmac_test_vectors[] =
 
 void test_sha_256()
 {
-    SHA256_CTX context;
+    LIBBTC_SHA256_CTX context;
     uint8_t buf[SHA256_DIGEST_LENGTH];
     uint8_t* digest_out; /* use non thread save buffer (optimized for embedded systems) */
     int oLen;
@@ -446,7 +446,7 @@ void test_sha_256()
 
 void test_sha_512()
 {
-    SHA512_CTX context;
+    LIBBTC_SHA512_CTX context;
     uint8_t buf[SHA512_DIGEST_LENGTH];
     uint8_t* digest_out; /* use non thread save buffer (optimized for embedded systems) */
     int oLen;
